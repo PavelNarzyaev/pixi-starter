4cb0cdb2869a2ba7e3c7c50b2a440360b1004440
Pavel Narzyaev <pavel@narzyaev.ru>
2019-12-11 03:31:20 +0300

    added vertical arrows

--- src/MainContainer.ts
+++ src/MainContainer.ts
@@ -3,6 +3,7 @@ import Arrows from "./Arrows";
 
 export default class MainContainer extends View {
 	private _horizontalArrows:Arrows;
+	private _verticalArrows:Arrows;
 
 	constructor() {
 		super();
@@ -11,6 +12,7 @@ export default class MainContainer extends View {
 	protected init():void {
 		super.init();
 		this.initHorizontalArrows();
+		this.initVerticalArrows();
 	}
 
 	private initHorizontalArrows():void {
@@ -18,13 +20,26 @@ export default class MainContainer extends View {
 		this.addChild(this._horizontalArrows);
 	}
 
+	private initVerticalArrows():void {
+		this._verticalArrows = new Arrows("100%");
+		this._verticalArrows.rotation = -Math.PI / 2;
+		this.addChild(this._verticalArrows);
+	}
+
 	protected applySize():void {
 		super.applySize();
 		this.alignHorizontalArrows();
+		this.alignVerticalArrows();
 	}
 
 	private alignHorizontalArrows():void {
 		this._horizontalArrows.setW(this.w);
-		this._horizontalArrows.y = this.h / 2;
+		this._horizontalArrows.y = this.h - this._horizontalArrows.h - 30;
+	}
+
+	private alignVerticalArrows():void {
+		this._verticalArrows.setW(this.h);
+		this._verticalArrows.x = this.w - this._horizontalArrows.h - 30;
+		this._verticalArrows.y = Math.floor(this.h);
 	}
 }
